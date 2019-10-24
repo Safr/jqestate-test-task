415ee1d77fee5813549db9325cf1cda7f4a03ad0
dpo3etka <djpo3etka@gmail.com>
2019-10-25 00:13:44 +0300

    Updates

---
+++ precache-manifest.2a6f247b3b2024b7d64217e80a155f0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871589d21e11bd7d4a2b1f9e0dbb9056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a3ea622755d1fee84d6",
+    "url": "/static/css/3.0a3aff7e.chunk.css"
+  },
+  {
+    "revision": "57c12e67678d231b2597",
+    "url": "/static/css/main.82744ec6.chunk.css"
+  },
+  {
+    "revision": "8b4cff7301f7f54890de",
+    "url": "/static/js/2.6d13ea6d.chunk.js"
+  },
+  {
+    "revision": "1a3ea622755d1fee84d6",
+    "url": "/static/js/3.a28e6e6b.chunk.js"
+  },
+  {
+    "revision": "57c12e67678d231b2597",
+    "url": "/static/js/main.3acb87b3.chunk.js"
+  },
+  {
+    "revision": "0ad51db5ef2d9c04269a",
+    "url": "/static/js/runtime-main.d1e4e6b2.js"
+  }
+]);
